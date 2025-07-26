7518c0076dd3510d0240938f290a89d10683fa7f
Alicamara20 <ali.camara1955@gmail.com>
2025-07-26 05:39:20 +0100

    update my components

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,7 +3,6 @@ import { images, projects } from "../Data/data"
 import Header from "../Header/Header"
 import Home from "../Home/HomePage"
 import Layout from "./Layout"
-import ModalContainer from "../Modal/ModalContainer"
 import Projects from "../Projects/ProjectsPage"
 function App() {
 
