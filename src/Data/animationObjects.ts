50fab47126b1bb75285bd001f958650fdc51b923
Ali <ali.camara1955@gmail.com>
2025-07-29 21:45:37 +0000

    add Data folder

---
+++ src/Data/animationObjects.ts
@@ -0,0 +1,52 @@
+import { delay } from "framer-motion/dom"
+
+ export const parentVariants = {
+    hidden: {opacity: 0},
+    show: {opacity: 1, 
+        transition: {
+            staggerChildren: 0.25,
+            duration: 5
+        }
+    }
+}
+
+export const navVariant = {
+    hidden:{ opacity: 0},
+    show: { opacity: 1},
+    transition: {
+      staggerChildren: 2
+    }
+    
+}
+export const aboutPageVariants = {
+    hidden: {opacity: 0},
+    show: {opacity: 1, 
+        transition: {
+            duration: 3
+        }
+    }
+}
+
+export const childrenVariant = {
+    hidden: {opacity: 0},
+    show: {opacity: 1}
+}
+export const skillsPageVariants = {
+    hidden: {opacity: 0},
+    show: {opacity: 1, 
+        transition: {
+            staggerChildren: .29,
+            
+        }
+    }
+}
+
+export const homePageVariants = {
+    hidden: {opacity: 0},
+    show: {opacity: 1, 
+        transition: {
+            duration: 3
+        }
+    }
+}
+
